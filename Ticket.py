df1b913645034f2e3cf85bdc1125dbc8bb789d89
miapillote <miapillote@gmail.com>
2025-03-09 12:27:44 -0400

    repackaged

---
+++ Ticket.py
@@ -0,0 +1,11 @@
+import FormReader
+
+class Ticket:
+    def __init__(self, pdf):
+        """
+        Data Key:
+        
+        
+        
+        """
+        data = FormReader.FormReader(pdf)
