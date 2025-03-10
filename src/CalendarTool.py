977b63de3477dda9eba79a5b3ff28d5110b751d6
miapillote <miapillote@gmail.com>
2025-03-10 14:23:56 -0400

    added calendar event module: working

--- src/CalendarTool.py
+++ src/CalendarTool.py
@@ -9,8 +9,7 @@ https://karenapp.io/articles/how-to-automate-google-calendar-with-python-using-t
 I need to switch the project to a different account so I can add rettnerhelpdesk as a test user...?
 
 """
-
-import datetime
+import Ticket
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -20,6 +19,31 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 CREDENTIALS_FILE = '../resources/secrets/credentials.json'
+PICKLE = '../resources/secrets/token.pickle'
+
+# Event parameter constants
+TIMEZONE = "America/New_York"
+LOCATION = "Rettner Hall Room 201"
+CALENDAR_ID = "primary"
+
+
+def create_event(ticket: Ticket):
+    service = get_calendar_service()
+
+    event = {
+        'summary': ticket.item + " Rental",
+        'location': LOCATION,
+        'start': {
+            'dateTime': ticket.date.isoformat(),
+            'timeZone': TIMEZONE},
+        'end': {
+            'dateTime': ticket.due.isoformat(),
+            'timeZone': TIMEZONE},
+        'attendees': [{'email': ticket.email}]
+    }
+
+    event_result = service.events().insert(calendarId=CALENDAR_ID, body=event).execute()
+    return event_result
 
 
 def get_calendar_service():
@@ -27,8 +51,8 @@ def get_calendar_service():
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
+    if os.path.exists(PICKLE):
+        with open(PICKLE, 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
@@ -40,8 +64,15 @@ def get_calendar_service():
             creds = flow.run_local_server(port=0)
 
         # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
+        with open(PICKLE, 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
     return service
+
+
+def test():
+    ticket = Ticket.Ticket()
+    ticket.read_form("../resources/forms/mpillote.pdf")
+    event = create_event(ticket)
+    return event
--- src/Ticket.py
+++ src/Ticket.py
@@ -31,13 +31,15 @@ class Ticket:
         self.due = self.read_date(re.findall(r"DUE:\s*(.*?)(?=\s*NAME:)", text)[0])
         self.email = re.findall(r"EMAIL:\s*(.*?)(?=\s*ATTENDANT)", text)[0]
 
-    def read_name(self, full_name):
+    @staticmethod
+    def read_name(full_name):
         full_name = full_name.split()
         first_name = full_name[0]
         last_name = " ".join(full_name[1:])
         return [first_name, last_name]
 
-    def read_date(self, date_string):
+    @staticmethod
+    def read_date(date_string):
         date_string_cleaned = re.sub(r'(\d+)(st|nd|rd|th)', r'\1', date_string)
         date_format = "%A, %B %d %Y, %I:%M %p"
         return datetime.datetime.strptime(date_string_cleaned, date_format)
